7574e8e13d0908e9826a96678b843be9717fa436
Vincent Cabansag <vcabansag@gmail.com>
2013-02-10 16:13:21 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warstler::Application.config.secret_token = '94d0d2ced365c95d54d7d6a357e909cb1cf18f8b537600aeb85b89837efb2c13a490b80d9852e4424ca6d7fdcf613c2609cf3080867a85b136ad9f97e5327f04'
