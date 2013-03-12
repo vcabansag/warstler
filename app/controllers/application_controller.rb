42e13845eaa9f0b64541ac995282f2c133d91450
Vincent Cabansag <vcabansag@gmail.com>
2013-03-12 15:14:02 -0500

    Adding http authentication for staging server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  def authenticate
+    if Rails.env.staging?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["HTTP_USERNAME"] && password == ENV["HTTP_PASSWORD"]
+      end
+    end
+  end
+
 end
