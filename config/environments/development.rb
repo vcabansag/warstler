f1d484a06050ae7e68fc3e2f5a2c36e6bb7083a7
Vincent Cabansag <vcabansag@gmail.com>
2013-07-04 16:05:00 -0500

    Rails 4 updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,12 +6,12 @@ Warstler::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  # Configure eager_load
+  config.eager_load = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -24,10 +24,6 @@ Warstler::Application.configure do
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
   config.assets.compress = false
